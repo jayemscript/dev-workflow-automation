37c57a7609b9673783f13a0875b9cd099a97af8f
jayemscript <johnmarkpulmano052802@gmail.com>
2026-09-24 11:20:48 +0800

    updated

--- open-projects.bat
+++ open-projects.bat
@@ -1,12 +1,12 @@
 @echo off
 
 wt ^
-  new-tab --title "Auth Service" cmd /k "cd /d C:\the-meal-guides\nestjs-authentication-service" ; ^
-  new-tab --title "File Media Service" cmd /k "cd /d C:\the-meal-guides\nestjs-file-media-worker" ; ^
-  new-tab --title "Mailer Service" cmd /k "cd /d C:\the-meal-guides\nestjs-mailer-worker" ; ^
-  new-tab --title "Payment Gateway Service" cmd /k "cd /d C:\the-meal-guides\nestjs-payment-gateway-service" ; ^
-  new-tab --title "Core Service" cmd /k "cd /d C:\the-meal-guides\the-meal-guides-core-domain" ; ^
-  new-tab --title "Admin Service" cmd /k "cd /d C:\the-meal-guides\the-meal-guides-admin-bff" ; ^
-  new-tab --title "Merchant Service" cmd /k "cd /d C:\the-meal-guides\the-meal-guides-merchant-bff" ; ^
-  new-tab --title "Admin Dashboard" cmd /k "cd /d C:\the-meal-guides\the-meal-guides-admin-dashboard" ; ^
-  new-tab --title "Merchant Portal" cmd /k "cd /d C:\the-meal-guides\the-meal-guides-merchant-app"
+  new-tab --title "Auth Service" --suppressApplicationTitle cmd /k "cd /d C:\the-meal-guides\nestjs-authentication-service && make dev" ; ^
+  new-tab --title "File Media Service" --suppressApplicationTitle cmd /k "cd /d C:\the-meal-guides\nestjs-file-media-worker && make dev" ; ^
+  new-tab --title "Mailer Service" --suppressApplicationTitle cmd /k "cd /d C:\the-meal-guides\nestjs-mailer-worker && make dev" ; ^
+  new-tab --title "Payment Gateway Service" --suppressApplicationTitle cmd /k "cd /d C:\the-meal-guides\nestjs-payment-gateway-service && make dev" ; ^
+  new-tab --title "Core Service" --suppressApplicationTitle cmd /k "cd /d C:\the-meal-guides\the-meal-guides-core-domain && make dev" ; ^
+  new-tab --title "Admin Service" --suppressApplicationTitle cmd /k "cd /d C:\the-meal-guides\the-meal-guides-admin-bff && make dev" ; ^
+  new-tab --title "Merchant Service" --suppressApplicationTitle cmd /k "cd /d C:\the-meal-guides\the-meal-guides-merchant-bff && make dev" ; ^
+  new-tab --title "Admin Dashboard" --suppressApplicationTitle cmd /k "cd /d C:\the-meal-guides\the-meal-guides-admin-dashboard && make dev" ; ^
+  new-tab --title "Merchant Portal" --suppressApplicationTitle cmd /k "cd /d C:\the-meal-guides\the-meal-guides-merchant-app && make dev"
